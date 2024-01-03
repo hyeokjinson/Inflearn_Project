70cd4dddf4dbb5f2d329f5dd13d92b663a009b7b
hyeokjinson <hjson817@gmail.com>
2024-01-03 16:39:35 +0900

    수정 필요

---
+++ Section1_25.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+
+	cin >> n;
+	vector<int>s(n);
+
+	for (int i = 0; i < n; i++) {
+
+	}
+
+	return 0;
+}
