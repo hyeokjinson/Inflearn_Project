05dfc145b110b9d89a8eba8b123f45ef06714876
hyeokjinson <hjson817@gmail.com>
2024-01-05 10:17:15 +0900

    석차구하기

--- Section1_25.cpp
+++ Section1_25.cpp
@@ -4,13 +4,25 @@
 using namespace std;
 
 int main() {
-	int n;
+	int n,val;
 
 	cin >> n;
+	vector<int>a(n);
 	vector<int>s(n);
 
 	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+		s[i] = 1;
 
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j =1; j < n; j++) {
+			if (a[i] <a[j])s[i]++;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d ", s[i]);
 	}
 
 	return 0;
