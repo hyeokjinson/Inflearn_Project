c92f80267a39875dc69c7c1670b990eedc31489c
hyeokjinson <hjson817@gmail.com>
2024-01-08 11:17:03 +0900

    선택정렬

---
+++ Section1_32.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	int n,tmp,idx;
+	cin >> n;
+	vector<int>num(n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &num[i]);
+	}
+	
+	for (int i = 0; i < n; i++) {
+		idx = i;
+		for (int j = i + 1; j < n; j++) {
+			if (num[idx] > num[j]) {
+				idx = j;
+			}
+			tmp = num[i];
+			num[i] = num[idx];
+			num[idx] = tmp;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d ", num[i]);
+	}
+
+	return 0;
+}
