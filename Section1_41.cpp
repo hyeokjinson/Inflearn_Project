fae040bfa4a57a8ee17e55835e683ed013315923
hyeokjinson <hjson817@gmail.com>
2024-01-16 09:04:22 +0900

    연속된 자연수의 합

--- Section1_41.cpp
+++ Section1_41.cpp
@@ -5,5 +5,24 @@
 using namespace std;
 
 int main() {
+	int a, b = 1, tmp = 0,cnt=0;
+	int i;
 
+	scanf("%d", &a);
+	tmp = a;
+	a--;
+	
+	while (a > 0) {
+		b++;
+		a = a - b;
+		if (a% b == 0) {
+			for (i = 1; i < b; i++) {
+				printf("%d + ", (a / b) + i);
+			}
+			printf("%d = %d\n", (a / b) + i, tmp);
+			cnt++;
+		}
+	}
+	printf("%d", cnt);
+	return 0;
 }
