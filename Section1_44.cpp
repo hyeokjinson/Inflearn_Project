8329ee9e5c75375386a548b086337b1196f03bc1
hyeokjinson <hjson817@gmail.com>
2024-01-19 09:19:49 +0900

    마구간 정하기

--- Section1_44.cpp
+++ Section1_44.cpp
@@ -3,9 +3,18 @@
 #include<algorithm>
 using namespace std;
 int n, c;
-
+int count(int len, int x[]) {
+	int cnt=1,pos = x[0];
+	for (int i = 1; i < n; i++) {
+		if (x[i] - pos >= len) {
+			cnt++;
+			pos = x[i];
+		}
+	}
+	return cnt;
+}
 int main() {
-	int i,start=0,end=0,mid;
+	int i,start=0,end=0,mid,res;
 	scanf("%d %d", &n, &c);
 	int* a = new int[n + 1];
 
@@ -17,6 +26,15 @@ int main() {
 	while (start <= end) {
 		mid = (start + end) / 2;
 
-		if()
+		if (count(mid, a) >= c) {
+			res = mid;
+			start = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
 	}
+	printf("%d", res);
+	delete[] a;
+	return 0;
 }
