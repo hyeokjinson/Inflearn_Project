d957d7e57edff9f72fa5989aa29864c912a5fc14
hyeokjinson <hjson817@gmail.com>
2024-02-05 10:30:37 +0900

    K진수 출력(stack 사용)

--- Section_53.cpp
+++ Section_53.cpp
@@ -1,9 +1,26 @@
 #include<cstdio>
+int stack[1001];
+int top = -1;
+void push(int x) {
+    stack[++top] = x;
+}
+int pop() {
+    return stack[top--];
+}
+
 
 int main(){
-    int n;
-    scanf("%d",&n);
-    printf("%d",n);
+    int n,k;
+    scanf("%d %d",&n,&k);
+    char str[20] = "0123456789ABCDEF";
+    
+    while (n > 0) {
+        push(n % k);
+        n = n / k;
+    }
+    while (top != -1) {
+        printf("%c", str[pop()]);
+    }
     
     return 0;
 }
